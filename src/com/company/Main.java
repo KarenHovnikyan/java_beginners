d1af46f50ee3450f34d294091a9eda68b691df66
KarenHovnikyan <hovnikyank@gmail.com>
2022-12-27 19:24:05 +0400

    add 3 System out printl "K H"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("K H");
+        System.out.println("K H");
+        System.out.println("K H");
     }
 }
 
