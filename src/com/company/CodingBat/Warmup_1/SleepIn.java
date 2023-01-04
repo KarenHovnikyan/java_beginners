7f139831fcb89faf859c546b025820cb193de500
KarenHovnikyan <hovnikyank@gmail.com>
2023-01-04 22:04:25 +0400

    change class Sleep In

--- src/com/company/CodingBat/Warmup_1/SleepIn.java
+++ src/com/company/CodingBat/Warmup_1/SleepIn.java
@@ -1,4 +1,17 @@
 package com.company.CodingBat.Warmup_1;
 
 public class SleepIn {
+    public static boolean sleep(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+       boolean result =  sleep(true,true);
+
+       System.out.println(result);
+    }
 }
