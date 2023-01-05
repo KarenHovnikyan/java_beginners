d116c4c6b61db03889fe0785971ab399f1a2e500
KarenHovnikyan <hovnikyank@gmail.com>
2023-01-05 18:26:57 +0400

    Create new Method in Logic directori

---
+++ src/com/company/CodingBat/Logic/MakeBricks.java
@@ -0,0 +1,14 @@
+package com.company.CodingBat.Logic;
+
+public class MakeBricks {
+
+    public boolean makeBricks(int small, int big, int goal) {
+        return !((big * 5 + small - goal < 0) || (((big * 5 + small - goal) % 5) - small > 0));
+    }
+
+        public static void main (String[]args){
+            MakeBricks makeBricks = new MakeBricks();
+            System.out.println(makeBricks.makeBricks(3, 2, 10));
+        }
+    }
+
