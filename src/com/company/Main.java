58ba419ee09cbb1bd196cb887010a9af661ea192
KarenHovnikyan <hovnikyank@gmail.com>
2023-01-05 19:15:03 +0400

    Create new Method in Logic directori

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,10 @@
 package com.company;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println(Arrays.toString(seriesUp(4)));
-
+        System.out.println("Hello");
     }
 
-    public static int[] seriesUp(int n) {
-        int[] numArray = new int[n*(n+1)/2];
-        for (int i = 0, k = 1, l = 1; i < numArray.length; i++) {
-            numArray[i] = k;
-            if(k == l) {   k = 1; l++;
-            } else {
-                k++;
-            }
-
-        }
-
-        return numArray;
-    }
 }
 
